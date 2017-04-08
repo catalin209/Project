6df004276023a36b2d5ce8e8f53f5a8d37e71099
catalin209 <zevedei.gheorghe@yahoo.com>
2017-04-08 13:13:23 +0300

    Networking

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	public Camera standbyCamera;
+	SpawnSpot[] spawnSpots;
+
+	// Use this for initialization
+	void Start () {
+		spawnSpots = GameObject.FindObjectsOfType<SpawnSpot>();
+		Connect ();
+	}
+
+	void Connect() {
+		PhotonNetwork.ConnectUsingSettings( "MultiFPS v001" );
+	}
+	 
+	void OnGUI() {
+		GUILayout.Label( PhotonNetwork.connectionStateDetailed.ToString() );
+	}
+
+	void OnJoinedLobby() {
+		Debug.Log ("OnJoinedLobby");
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	void OnPhotonRandomJoinFailed() {
+		Debug.Log ("OnPhotonRandomJoinFailed");
+		PhotonNetwork.CreateRoom( null );
+	}
+
+	void OnJoinedRoom() {
+		Debug.Log ("OnJoinedRoom");
+
+		SpawnMyPlayer();
+	}
+
+	void SpawnMyPlayer() {
+		if(spawnSpots == null) {
+			Debug.LogError ("WTF?!?!?");
+			return;
+		}
+
+		SpawnSpot mySpawnSpot = spawnSpots[ Random.Range (0, spawnSpots.Length) ];
+		GameObject myPlayerGO = (GameObject)PhotonNetwork.Instantiate("PlayerController", mySpawnSpot.transform.position, mySpawnSpot.transform.rotation, 0);
+		standbyCamera.enabled = false;
+
+		((MonoBehaviour)myPlayerGO.GetComponent("FPSInputController")).enabled = true;
+		((MonoBehaviour)myPlayerGO.GetComponent("MouseLook")).enabled = true;
+		myPlayerGO.transform.FindChild("Main Camera").gameObject.SetActive(true);
+	}
+}
